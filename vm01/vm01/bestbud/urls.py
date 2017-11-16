c1e0ac8c9a30e8690acbf3076f6fa3eebbd06ac0
root <jardelsantos44@gmail.com>
2017-11-16 20:34:48 -0200

    Confirmacao de Compra

--- vm01/vm01/bestbud/urls.py
+++ vm01/vm01/bestbud/urls.py
@@ -5,8 +5,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^check_age/$', TemplateView.as_view(template_name='bestbud/check_age.html')),
     url(r'^check_age/$', views.check_age, name='check_age'),
     #url(r'^boas-vindas/$', views.boas_vindas, {'template_name': 'bestbud/boas-vindas.html'}, name='boas-vindas'),
-    url(r'^check_age/produtos/$', views.produtos, name='produtos'),
+    url(r'^produtos/$', views.produtos, name='produtos'),
+    url(r'^buy/$', views.buy, name='buy'),
+
+
 ]
--- vm01/vm01/bestbud/views.py
+++ vm01/vm01/bestbud/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from bestbud.watchdog import Watchdog
 import time
-
+import numpy as np
 # Create your views here.
 
 def index(request):
@@ -13,7 +13,7 @@ def check_age(request):
     #return render(request, 'bestbud/check_age.html')
     #anterior = render(request, 'bestbud/index.html')
     #timer.sleep(5)
-    return render(request, 'bestbud/index.html')
+    return render(request, 'bestbud/check_age.html')
 
     #while not time.sleep(5) == True:
         #return render(request, 'bestbud/check_age.html')
@@ -24,9 +24,9 @@ def check_age(request):
             #while(flag == 0):
                 #if flag == 1:
                     #wd.stop()
-        return render(request, 'bestbud/index.html')
+        return render(request, 'bestbud/index')
     except Exception as e:
-        return render(request, 'bestbud/index.html')
+        return render(request, 'bestbud/index')
     """
 
 #def check_age(request):
@@ -34,3 +34,19 @@ def check_age(request):
 
 def produtos(request):
     return render(request, 'bestbud/products.html')
+
+def buy(request):
+    produtos = request.POST.getlist('prod')
+    lista = ['CocaCola', 'Pepsi', 'Soda', 'Guarana',
+    'CocaCola', 'Pepsi', 'Soda', 'Guarana',
+    'CocaCola', 'Pepsi', 'Soda', 'Guarana',
+    'CocaCola', 'Pepsi', 'Soda', 'Guarana',
+    'CocaCola', 'Pepsi', 'Soda', 'Guarana',
+    'CocaCola', 'Pepsi', 'Soda', 'Guarana',
+    'CocaCola', 'Pepsi', 'Soda', 'Guarana',
+    'CocaCola', 'Pepsi', 'Soda', 'Guarana']
+    compras = []
+    for i in range(32):
+        if int(produtos[i])>0:
+            compras.append('Produto %s: %d' % (lista[i],int(produtos[i])))
+    return render(request, 'bestbud/buy.html', {'compras': compras})
