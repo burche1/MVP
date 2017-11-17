0f1c7d4a44652292ef2d2c1a0145781d007231d8
root <jardelsantos44@gmail.com>
2017-11-17 01:34:44 -0200

    Create Database

--- vm01/vm01/bestbud/models.py
+++ vm01/vm01/bestbud/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.FloatField(default=0)
+    def __str__(self):
+        return self.name
+
+class Purchase(models.Model):
+    product = models.ForeignKey(Product,  on_delete=models.CASCADE)
+    date = models.DateTimeField('date purchased')
+    qtd = models.IntegerField(default=1)
+    purchase_id = models.IntegerField(default=0)
+    def __str__(self):
+        return 'Compra ' + str(self.purchase_id)
