7013faaa674abffa14579d30ab8abb6b96324513
root <jardelsantos44@gmail.com>
2017-11-18 03:00:47 -0200

    Ajustes

--- vm01/vm01/bestbud/models.py
+++ vm01/vm01/bestbud/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
+    logo = models.ImageField(upload_to='bestbud/static/images')
     name = models.CharField(max_length=50)
-    price = models.FloatField(default=0)
+    price = models.DecimalField(default=0,max_digits=5,decimal_places=2)
     def __str__(self):
         return self.name
 
